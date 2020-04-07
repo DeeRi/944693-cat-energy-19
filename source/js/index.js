c188db09f2e79997730d0b5ea074ae1eacf01927
Alena Zueva <aleo.zueva@gmail.com>
2020-04-07 22:21:17 +0700

    Fixes menu, adds styles to index and background for promo

--- source/js/index.js
+++ source/js/index.js
@@ -1,12 +1,12 @@
 let burger = document.querySelector(".menu-button");
-let mainNav = document.querySelector(".header__navigation");
+let siteNav = document.querySelector(".header__navigation");
 
 burger.addEventListener("click", function() {
   if (burger.classList.contains("menu-button--clicked")) {
     burger.classList.remove("menu-button--clicked");
-    mainNav.classList.add("header__navigation--open");
+    siteNav.classList.remove("header__navigation--open");
   } else {
     burger.classList.add("menu-button--clicked");
-    mainNav.classList.remove("header__navigation--open");
+    siteNav.classList.add("header__navigation--open");
   }
 });
