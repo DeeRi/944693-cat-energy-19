2a847c027b845448ee468a17fece80b6d23b22cc
Alena Zueva <aleo.zueva@gmail.com>
2020-04-11 17:14:00 +0700

    Adds map

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,24 @@
+let myMap;
+
+ymaps.ready(init);
+
+function init() {
+  myMap = new ymaps.Map(
+    "map",
+    {
+      center: [59.938631, 30.323055],
+      zoom: 15
+    },
+    {
+      searchControlProvider: "yandex#search"
+    }
+  );
+}
+
 let burger = document.querySelector(".menu-button");
 let siteNav = document.querySelector(".header__navigation");
 
-burger.addEventListener("click", function() {
+burger.addEventListener("click", function () {
   if (burger.classList.contains("menu-button--clicked")) {
     burger.classList.remove("menu-button--clicked");
     siteNav.classList.remove("header__navigation--open");
